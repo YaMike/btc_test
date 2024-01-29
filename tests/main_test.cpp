69f89c117df8f67c45a9b944ef1696260d98775e
Mikhail Likholet <Mikhail.Likholet@onsemi.com>
2024-01-29 15:55:22 +0400

    added main test

---
+++ tests/main_test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "OrderBook.hpp"
+
+TEST(OrderBookTest, ProcessSnapshotAndUpdates) {
+
+    OrderBook orderBook;
+
+    Snapshot snapshot = {
+        .bids={{0.0024, 14.70000000}, {0.0022, 6.40000000}, {0.0020, 9.70000000}},
+        .asks={{0.0024, 14.90000000}, {0.0026, 3.60000000}, {0.0028, 1.00000000}}
+    };
+
+    orderBook.processSnapshot(snapshot);
+    EXPECT_EQ(orderBook.getBestBid(), 0.0024);
+    EXPECT_EQ(orderBook.getBestAsk(), 0.0026);
+
+    std::vector<Update> updates = {
+        {"depthUpdate", "BNBBTC", {{"b", {{0.0024, 10.0  }}}, {"a", {{0.0026, 100.0 }}}}},
+        {"depthUpdate", "BNBBTC", {{"b", {{0.0024, 8.0   }}}, {"a", {{0.0028, 0.0   }}}}},
+        {"depthUpdate", "BNBBTC", {{"b", {{0.0024, 0.0   }}}, {"a", {{0.0026, 15.0}, {0.0027, 5.0} }}}},
+        {"depthUpdate", "BNBBTC", {{"b", {{0.0025, 100.0 }}}, {"a", {{0.0026, 0.0}, {0.0027, 5.0}  }}}},
+        {"depthUpdate", "BNBBTC", {{"b", {{0.0025, 0.0   }}}, {"a", {{0.0026, 15.0}, {0.0024, 0.0} }}}}
+    };
+
+    for (const auto& update : updates) {
+        orderBook.processUpdate(update);
+    }
+    EXPECT_EQ(orderBook.getBestBid(), 0.0024);
+    EXPECT_EQ(orderBook.getBestAsk(), 0.0028);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
